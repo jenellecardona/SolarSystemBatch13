fefd537d93073b75dd2dc3b3b19aed778eb6548c
jenellecardona <jnel.cardona@gmail.com>
2022-09-02 07:12:19 -0400

    Solar system ready to be deployed

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        SolarSystem solar=new SolarSystem();
+        solar.Sun=1;
+        solar.Moon=4;
+        solar.Planet=2;
+
+        System.out.println("The basic structure is ready");
+
+    }
+}
--- src/com/company/SolarSystem.java
+++ src/com/company/SolarSystem.java
@@ -1,9 +1,8 @@
 package com.company;
 
 public class SolarSystem {
-    public static void main(String[] args) {
         int Sun;
         int Moon;
         int Planet;
-    }
+
 }
